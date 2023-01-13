5a2bd45757380cb49479d301aa57a13798cab6e8
Anna <lamafon@mail.ru>
2023-01-13 22:48:23 +0300

    fix: done

--- src/index.js
+++ src/index.js
@@ -29,11 +29,13 @@ module.exports = function toReadable (number) {
         case 14:
             return 'fourteen';
         case 15:
-            return 'fifteen'
+            return 'fifteen';
+        case 18:
+            return 'eighteen';
     }
     if (splitNumber.length < 2) {
         return units[splitNumber[0] - 1]
-    } else if (digit > 16 && digit < 20) {
+    } else if (digit > 15 && digit < 20) {
         return units[splitNumber.pop() - 1] + 'teen';
     } else if (splitNumber.length < 3 && splitNumber[splitNumber.length - 1] === 0) {
         return dozens[splitNumber[0] - 1];
@@ -41,10 +43,10 @@ module.exports = function toReadable (number) {
         return `${dozens[splitNumber[0] - 1]} ${units[splitNumber[1] - 1]}`
     } else if (splitNumber.length < 4) {
         if (splitNumber[1] === 1) {
-            return `${units[splitNumber[0] - 1]} hundred ${tens[splitNumber[2]]}`;
+            return (`${units[splitNumber[0] - 1]} hundred ${tens[splitNumber[2]]}`).replace('undefined ', '');
          } else if (splitNumber[splitNumber.length - 1] === 0) {
             splitNumber.pop()
-            return (`${units[splitNumber[0] - 1]} hundred ${dozens[splitNumber[1] - 1]}`).replace('undefined ', '');
+            return (`${units[splitNumber[0] - 1]} hundred ${dozens[splitNumber[1] - 1]}`).replace(' undefined', '');
         } else {
             return (`${units[splitNumber[0] - 1]} hundred ${dozens[splitNumber[1] - 1]} ${units[splitNumber[2] - 1]}`).replace('undefined ', '');
         }
